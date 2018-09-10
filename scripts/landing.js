125661fd8292bca054ca5a532c098caf8422d485
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2018-09-09 22:15:14 -0700

    refactored landing.js. also changed transition on .point class

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,6 +2,18 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
+  var revealPoint = function() {
+    for (var i=0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+  };
+  revealPoint();
+};
+
+/*
   var revealFirstPoint = function() {
       points[0].style.opacity = 1;
       points[0].style.transform = "scaleX(1) translateY(0)";
@@ -26,5 +38,4 @@ var animatePoints = function() {
   revealFirstPoint();
   revealSecondPoint();
   revealThirdPoint();
-
-};
+*/
