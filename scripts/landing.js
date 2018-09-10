e6bd314fa5ae72b88c6f00a198f4a9b21607d3be
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2018-09-09 23:38:04 -0700

    added event when user scrolls down, points will appear

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
   var revealPoint = function() {
     for (var i=0; i < points.length; i++) {
       points[i].style.opacity = 1;
@@ -12,7 +11,21 @@ var animatePoints = function() {
   };
   revealPoint();
 };
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
+  window.addEventListener('scroll', function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+      }
+  });
+}
 /*
   var revealFirstPoint = function() {
       points[0].style.opacity = 1;
