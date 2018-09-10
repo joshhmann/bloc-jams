e3ded7337cd36380444017446c5a7236d99236e2
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2018-09-10 16:23:15 -0700

    created utilities.js, made a forEach function to loop through elements and created a callback

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,21 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function() {
-    for (var i=0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-  revealPoint();
-};
+var animatePoints = function(points) {
+  forEach(points, revealPoint);
+  };
+
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
@@ -26,29 +25,3 @@ window.onload = function() {
       }
   });
 }
-/*
-  var revealFirstPoint = function() {
-      points[0].style.opacity = 1;
-      points[0].style.transform = "scaleX(1) translateY(0)";
-      points[0].style.msTransform = "scaleX(1) translateY(0)";
-      points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealSecondPoint = function() {
-      points[1].style.opacity = 1;
-      points[1].style.transform = "scaleX(1) translateY(0)"
-      points[1].style.msTransform = "scaleX(1) translateY(0)";
-      points[1].style.WebkitTransform = "scale(1) translateY(0)";
-  };
-
-  var revealThirdPoint = function() {
-      points[2].style.opacity = 1;
-      points[2].style.transform = "scaleX(1) translateY(0)"
-      points[2].style.msTransform = "scaleX(1) translateY(0)";
-      points[2].style.WebkitTransform = "scale(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-*/
