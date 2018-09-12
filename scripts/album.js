8de74f164864ca93466264eac17544925bd20a52
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2018-09-11 21:53:32 -0700

    added a eventlistener where clicking the album image changes artist

--- scripts/album.js
+++ scripts/album.js
@@ -20,13 +20,35 @@ var albumMarconi = {
   artist: 'Guglielmo Marconi',
   label: 'EM',
   year: '1909',
-  albumArtUrl: 'assets/images/album_cover/20.png',
+  albumArtUrl: 'assets/images/album_cover/02.png',
   songs: [
     { title: 'Hello, Operator?', duration: '1:01' },
     { title: 'Ring, ring, ring', duration: '5:01' },
     { title: 'Fits in your pocket', duration: '3:21' },
     { title: 'Can you hear me now?', duration: '3:14' },
     { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+//third example album
+
+var albumAlex = {
+  title: 'Humbug',
+  artist: 'Arctic Monkeys',
+  label: 'Domino',
+  year: '2009',
+  albumArtUrl: 'assets/images/album_cover/21.png',
+  songs: [
+    { title: 'My Propeller', duration: '3:27' },
+    { title: 'Crying Lightning', duration: '3:43' },
+    { title: 'Dangerous Animals', duration: '3:30' },
+    { title: 'Secret Door', duration: '3:43' },
+    { title: 'Potion Approaching', duration: '3:32'},
+    { title: 'Fire and the Thud', duration: '3:57' },
+    { title: 'Cornerstone', duration: '3:18' },
+    { title: 'Dance Little Liar', duration: '4:43' },
+    { title: 'Pretty Visitors', duration: '3:40' },
+    { title: 'The Jeweller Hands', duration: '3:32'}
   ]
 };
 
@@ -42,13 +64,13 @@ var createSongRow = function(songNumber, songName, songLength) {
   return template;
 };
 
-var setCurrentAlbum = function(album) {
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+var setCurrentAlbum = function(album) {
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -63,4 +85,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  var albums = [albumPicasso, albumMarconi, albumAlex];
+  var index = 1;
+  albumImage.addEventListener("click", function(event){
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index == albums.length) {
+        index = 0;
+      }
+  });
 };
